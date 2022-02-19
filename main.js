5d3ed9eb6de6f50aeb4c10a58063ceb7c454a853
earthbender159 <80174685+earthbender159@users.noreply.github.com>
2022-02-19 13:54:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(640,480); 
+    canvas.position(110,250);
+}
+function draw(){
+    circle(100, 100, 100);
+    fill(13, 112, 40); 
+    rect(30, 20, 55, 56);
+    fill(13, 112, 81); 
+    ellipse(201, 102, 55, 55);
+    fill(18, 128, 150); 
+
+}
+function take_snapshot(){
+    
+}
